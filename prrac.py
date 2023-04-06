30b91d6c398484b7cc697f6fad4aaaa2765574c1
lakshya1-git <lakshyapandey961@gmail.com>
2023-04-06 19:27:45 +0530

    firstcommit

--- prrac.py
+++ prrac.py
@@ -1,12 +1,13 @@
 class car:
-    def __int__(self, body, engin, tyre):
+    def __init__(self, body, engin, tyre):
         self.body = body
         self.engin = engin
-        self. tyre = tyre
+        self.tyre = tyre
     def mailage():
         print("Milage of a car")
 c = car("second_hand", "v5", "mrf")
 class tata(car):
-    pass
-t = ("body", "engin", " tyre")
+        pass
+t = tata("body", "engin", " tyre")
 print(t)
+print(t.milage())
