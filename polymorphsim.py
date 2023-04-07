fb5a1bfaff7931a31176a66e5944882770c42e7a
lakshya1-git <lakshyapandey961@gmail.com>
2023-04-07 15:49:25 +0530

    firstcommit

---
+++ polymorphsim.py
@@ -0,0 +1,10 @@
+class iNeruron:
+    def __init__(self):
+        print("Print student detail")
+class class_type:
+    def class_for_student(self):
+        print("printthe class typpe of student")
+def test(a,b):
+    return a+b
+print(test(3,4))
+print(test("laksha", "pandey"))
