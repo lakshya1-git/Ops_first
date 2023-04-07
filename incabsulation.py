2134fb0adde84cfe90af06e178489260cc1d2c1c
lakshya1-git <lakshyapandey961@gmail.com>
2023-04-07 15:42:59 +0530

    firstommit

---
+++ incabsulation.py
@@ -0,0 +1,21 @@
+class iNeuron:
+    def __init__(self):
+        self.__students1 = "data science"
+    
+    def students(self):
+        print(self.__students1)
+    
+    def change_new(self, new_value):
+        self.__students1 = new_value
+
+# Instantiate the class
+i = iNeuron()
+
+# Call the students() method
+i.students() # Output: "data science"
+
+# Update the value of __students1 using the change_new() method
+i.change_new("value changed")
+
+# Call the students() method again to see the updated value
+i.students()
