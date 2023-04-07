21080893e78069fddec5b645c4a83519242482ea
lakshya1-git <lakshyapandey961@gmail.com>
2023-04-07 14:48:30 +0530

    firstcommit

---
+++ incabs_check.py
@@ -0,0 +1,8 @@
+class ineuron:
+    def __init__(self):
+        self.students1 = "data science"
+    def students(self):
+        print(self.students1)
+i = ineuron()
+i.students()
+i.student1 = "data analytics"
