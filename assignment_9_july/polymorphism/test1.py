beb49edadae63fd7db29f768af69dc20dcaf1b66
lakshya1-git <lakshyapandey961@gmail.com>
2023-04-08 16:45:15 +0530

    firstcommit

---
+++ assignment_9_july/polymorphism/test1.py
@@ -0,0 +1,10 @@
+class iNeuron:
+    def students(self):
+        print("print data of students")
+class class_type:
+    def class_for_students(self):
+        print("Access data of students")
+def add(a,b):
+    return a+b
+print(add(3,4))
+print(add("lakshya", "pandey"))
